3abe93f5a85e77e544a899c9627dbf7aafc3f764
Ebon <ivanrios072@gmail.com>
2021-01-25 17:10:37 -0700

    Added Movement Script and CameraFollow Script

--- Prototype 1 - Driving Sim/Assets/Scripts/Move.cs
+++ Prototype 1 - Driving Sim/Assets/Scripts/Move.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float hInput;
+    public float fInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0,0,5);
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
     }
 }
